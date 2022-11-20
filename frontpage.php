8a912d1a9c23236e2ed8ad27769b06ac1d99b3c3
Raquel <raqueloide12@gmail.com>
2022-11-20 21:03:45 +0000

    Front page finish

--- frontpage.php
+++ frontpage.php
@@ -5,18 +5,21 @@
 </head>
 <body>
     <div id="gallery">
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
+        <?php
+        include("config.php");
+        $idproduct = array('101', '102', '106', '107', '108', '109', '110', '111', '112', '113', '114', '115');
+        
+        foreach ($idproduct as $id){
+            $query=$conn->query("select image_url from Image where idProductFK='$id'");
+            while($entry = $query->fetch_assoc()){
+                $image = $entry["image_url"];
+            }
+            echo "<div class='tile' id='galery-item-$id' prodid='$id'><img src='{$image}'></div>";
+    }
+
+
+        $conn->close();
+        ?>
     </div>
 </body>
 </html>
